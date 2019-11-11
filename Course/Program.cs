0212986d7e74336915f1abef4326908bc12a797d
vinaspsy <vinaspsy@hotmail.com>
2019-11-11 20:45:10 -0300

    Herança

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Course.Entities;
 using System;
 using System.Globalization;
-using Course.Entities.Enums;
 
 
 namespace Course
@@ -11,44 +10,14 @@ namespace Course
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter client data:");
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Email: ");
-            string email = Console.ReadLine();
-            Console.Write("Birth date (DD/MM/YYYY): ");
-            DateTime birthdate = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine("Enter order data:");
-            Console.Write("Status: ");
-            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
 
-            Client client = new Client(name, email, birthdate);
-            Order order = new Order(DateTime.Now, status, client);
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
 
-            Console.Write("How many items to this order? ");
-            int n = int.Parse(Console.ReadLine());
-            for(int i = 1; i <= n; i++)
-            {
-                Console.WriteLine($"Enter #{i} item data:");
-                Console.Write("Product name: ");
-                string productName = Console.ReadLine();
-                Console.Write("Product price: ");
-                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.Write("Quantity: ");
-                int quantity = int.Parse(Console.ReadLine());
-
-                Product product = new Product(productName, productPrice);
-                OrderItem orderItem = new OrderItem(quantity, productPrice, product);
-
-                order.AddItem(orderItem);
-
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("ORDER SUMMARY:");
-            Console.WriteLine(order);
-
-
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
             
         }
     }
