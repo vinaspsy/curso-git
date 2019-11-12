8bc8fa9c3e296aeefc4606382c2449679fd375e3
vinaspsy <vinaspsy@hotmail.com>
2019-11-12 20:48:11 -0300

    Exercício de herança e polimorfismo

---
+++ Course/Entities/UsedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course.Entities
+{
+    class UsedProduct : Product
+    {
+
+        public DateTime ManufacturateDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufacturateDate) 
+            : base(name, price)
+        {
+            ManufacturateDate = manufacturateDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " (used) $ " + Price.ToString("F2", CultureInfo.InvariantCulture) + " (Manufacture date: "
+                + ManufacturateDate.ToString("dd/MM/yyyy") + ")";
+        }
+
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -10,40 +10,42 @@ namespace Course
         static void Main(string[] args)
         {
 
-            List<Employee> list = new List<Employee>();
+            List<Product> prod = new List<Product>();
 
-            Console.Write("Enter the number of employees: ");
+            Console.Write("Enter the number of products: ");
             int n = int.Parse(Console.ReadLine());
 
             for(int i = 1; i <= n; i++)
             {
-                Console.WriteLine($"Employee #{i} data: ");
-                Console.Write("Outsourced (y/n)? ");
-                char ch = char.Parse(Console.ReadLine());
+                Console.WriteLine($"Product #{i} data: ");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
                 Console.Write("Name: ");
                 string name = Console.ReadLine();
-                Console.Write("Hours: ");
-                int hours = int.Parse(Console.ReadLine());
-                Console.Write("Value per hour: ");
-                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                if (ch == 'y')
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (type == 'i')
                 {
-                    Console.Write("Additional charge: ");
-                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    prod.Add(new ImportedProduct(name, price, customsFee));
+
+                }else if(type == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    prod.Add(new UsedProduct(name, price, manufactureDate));
                 }
                 else
                 {
-                    list.Add(new Employee(name, hours, valuePerHour));
+                    prod.Add(new Product(name, price));
                 }
-
             }
 
             Console.WriteLine();
-            Console.WriteLine("PAYMENTS:");
-            foreach(Employee emp in list)
-            {
-                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product prods in prod) {
+                Console.WriteLine(prods.PriceTag());
             }
             
         }
