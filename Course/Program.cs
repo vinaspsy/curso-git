d3d8572e69ef29556c67c7184e87ab1e41c00b71
vinaspsy <vinaspsy@hotmail.com>
2019-11-22 20:12:46 -0300

    Exercício resolvido (Expressões lambda, delegates, LINQ)

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,35 +1,46 @@
 ﻿using System;
+using System.IO;
 using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
 using Course.Entities;
-using Course.Services;
 
 namespace Course
 {
+
     class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter contract data");
-            Console.Write("Number: ");
-            int contractNumber = int.Parse(Console.ReadLine());
-            Console.Write("Date (dd/MM/yyyy): ");
-            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
-            Console.Write("Contract value: ");
-            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Enter number of installments: ");
-            int months = int.Parse(Console.ReadLine());
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
 
-            Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+            List<Product> list = new List<Product>(); ;
 
-            ContractService contractService = new ContractService(new PaypalService());
-            contractService.ProcessContract(myContract, months);
-
-            Console.WriteLine("Installments:");
-            foreach (Installment installment in myContract.Installments)
+            using (StreamReader sr = File.OpenText(path))
             {
-                Console.WriteLine(installment);
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+
+                }
             }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
         }
+
     }
 }
